54713ffad4be0b50f9aff1da749b0274a2fdcf20
shoeb-howlader <mdshoebhawlader@gmail.com>
2021-07-11 10:17:22 +0600

    first commmit

---
+++ src/components/Results.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+<p >Reaction Time : {{score}}</p>
+<p class="rank">{{rank}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['score'],
+        data() {
+            return {
+                rank: null
+            }
+        },
+        mounted() {
+            if(this.score<250)
+            {
+                this.rank='ninja fingers'
+            }
+            else if (this.score<400)
+            {
+                this.rank='Rapid refelxes'
+            }
+            else
+            {
+                this.rank='Snail pace....'
+            }
+        },
+    }
+</script>
+
+<style >
+.rank{
+    font-size: 1.4em;
+    color: #0faf80;
+    font-weight: bold;
+}
+
+</style>
